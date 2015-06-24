1aa36a93d74534cf8bf576424b4cb18ff76f35be
KG <qianfeng@KG.local>
2015-06-24 14:42:26 +0800

    Initial Commit

---
+++ UITableViewCustomCellForXibDemo/KGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGAppDelegate.h
+//  UITableViewCustomCellForXibDemo
+//
+//  Created by KG on 15/6/24.
+//  Copyright (c) 2015年 KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewCustomCellForXibDemoTests/UITableViewCustomCellForXibDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  UITableViewCustomCellForXibDemoTests.m
+//  UITableViewCustomCellForXibDemoTests
+//
+//  Created by KG on 15/6/24.
+//  Copyright (c) 2015年 KG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITableViewCustomCellForXibDemoTests : XCTestCase
+
+@end
+
+@implementation UITableViewCustomCellForXibDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
